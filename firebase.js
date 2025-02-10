3fb886894116fb418a659318225dec9e904afdec
sushanshetty1 <sushanshetty1470@gmail.com>
2025-02-10 19:36:44 +0530

    Add Firebase configuration and update dependencies in package.json

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC7plFcNs4UDNmKwQlbX9GLum_RdtkGcn0",
+  authDomain: "pixelmart-1.firebaseapp.com",
+  projectId: "pixelmart-1",
+  storageBucket: "pixelmart-1.firebasestorage.app",
+  messagingSenderId: "756574164217",
+  appId: "1:756574164217:web:c1e3dd60333e54c2547e28",
+  measurementId: "G-JWT9SG6KW9"
+};
+
+const app = initializeApp(firebaseConfig);
